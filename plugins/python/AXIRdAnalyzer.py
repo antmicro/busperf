b97baa1b8a5d9a23230291233ef271bde808681d
Kamil Danecki <kdanecki@internships.antmicro.com>
2025-08-27 15:58:17 +0200

    Read response
    Signed-off-by: Kamil Danecki <kdanecki@internships.antmicro.com>

--- plugins/python/AXIRdAnalyzer.py
+++ plugins/python/AXIRdAnalyzer.py
@@ -3,14 +3,15 @@ class AXIRdAnalyzer:
         print("Loaded AXIRAnalyzer")
 
     def get_yaml_signals(self):
-        return ["ar_rdy", "ar_vld", "r_rdy", "r_vld"]
+        return ["ar_rdy", "ar_vld", "r_rdy", "r_vld", "r_resp"]
 
-    def analyze(self, clk, rst, ar_rdy, ar_vld, r_rdy, r_vld):
+    def analyze(self, clk, rst, ar_rdy, ar_vld, r_rdy, r_vld, r_resp):
         time_end = clk[-1][0]
         next_time = list(map(lambda r: r[0], ar_vld))
         next_time = next_time[2:]
         next_time.append(time_end)
         next_time.append(time_end)
+        r_resp.reverse()
         transactions = []
         
         for ((time, value), next_time) in zip(ar_vld, next_time):
@@ -21,8 +22,10 @@ class AXIRdAnalyzer:
             resp_time = first_data
             last_data = first_data
             delay = next_time - resp_time
+            resp = next(filter(lambda r: r[0] <= resp_time, r_resp))
+            print(resp)
 
-            transactions.append((time, resp_time, last_data, first_data, "01", delay))
+            transactions.append((time, resp_time, last_data, first_data, resp[1], delay))
         return transactions
 
 def create():
