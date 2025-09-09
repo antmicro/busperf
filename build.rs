254492a3abcc7d94bcfe444af928285c4e3a035a
Kamil Danecki <kdanecki@internships.antmicro.com>
2025-09-09 14:48:45 +0200

    Fix clippy
    Signed-off-by: Kamil Danecki <kdanecki@internships.antmicro.com>

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 use std::io::ErrorKind;
 
 fn create_symlink(plugins_dir: &str, out_dir: &str) {
-    match symlink::symlink_dir(&plugins_dir, &out_dir) {
+    match symlink::symlink_dir(plugins_dir, out_dir) {
         Ok(_) => (),
         Err(e) => {
             if e.kind() == ErrorKind::AlreadyExists {
-                symlink::remove_symlink_dir(&out_dir)
+                symlink::remove_symlink_dir(out_dir)
                     .expect("To update the symlink we should remove the old one");
-                symlink::symlink_dir(&plugins_dir, &out_dir)
+                symlink::symlink_dir(plugins_dir, out_dir)
                     .expect("Now we should be able to create the symlink");
             } else {
                 // Any other error should result in a panic!
