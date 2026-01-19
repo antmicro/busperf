5b10315977cded24ad84ff1524a31983a13192f8
Kamil Danecki <kdanecki@internships.antmicro.com>
2026-01-19 11:56:28 +0100

    Update CI
    * Remove wasm-bindgnen-cli installation because we use library crate
    * Remove wasm32-unknown-unknown target installation because it is
      automatically installed due to rust-toolchain.toml
    
    Signed-off-by: Kamil Danecki <kdanecki@internships.antmicro.com>

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -7,8 +7,6 @@ $APT_PREFIX apt update -qq
 $APT_PREFIX apt install -qqy curl gcc python3 libpython3-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &> /dev/null
 . ~/.cargo/env
-rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli --version 0.2.105
 
 # For checking all combination of features
 cargo install cargo-hack
--- ci/cargo_tests_deps.sh
+++ ci/cargo_tests_deps.sh
@@ -7,8 +7,6 @@ $APT_PREFIX apt update -qq
 $APT_PREFIX apt install -qqy curl gcc python3 python3-setuptools libpython3-dev python3-more-itertools git
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &> /dev/null
 . ~/.cargo/env
-rustup target add wasm32-unknown-unknown
-cargo install wasm-bindgen-cli --version 0.2.105
 
 # tuttest
 git clone https://github.com/antmicro/tuttest.git
